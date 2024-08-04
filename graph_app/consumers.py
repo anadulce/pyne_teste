c26e35e22f82debf26c514fc65d21b51f4a69dd0
Ana Dulce <ana.torres@jusbrasil.com.br>
2024-08-04 16:11:36 -0300

    Salvando contexto

--- graph_app/consumers.py
+++ graph_app/consumers.py
@@ -15,10 +15,11 @@ class GraphConsumer(AsyncWebsocketConsumer):
         query_string = self.scope.get('query_string', b'').decode()
         params = parse_qs(query_string)
         self.username = params.get('username', [''])[0]
+        self.width = int(params.get('width', [100])[0])
+        self.height = int(params.get('height', [100])[0])
         
-
-        # Add user node if not already present
-        await self.add_node(self.username)
+        # Cria o nó se ele já não existe
+        await self.add_node()
         
         await self.channel_layer.group_add(
             "graph",
@@ -36,51 +37,49 @@ class GraphConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
-        nodes = data.get('nodes', [])
-        links = data.get('links', [])
-        print("TESTEEEEEEEEEEE")
-
-        # Save updated state to database
         from ipdb import set_trace
         set_trace()
-        await self.save_graph_state(nodes, links)
+        data = json.loads(text_data)
+        # nodes = data.get('nodes', [])
+        # links = data.get('links', [])
+        target = data.get('target')
 
-        await self.channel_layer.group_send(
-            "graph",
-            {
-                'type': 'graph_update',
-                'data': {
-                    'nodes': nodes,
-                    'links': links
-                }
-            }
-        )
+        # Save updated state to database
+        if target:
+            await self.add_link(target)
+
+        # await self.channel_layer.group_send(
+        #     "graph",
+        #     {
+        #         'type': 'graph_update',
+        #         'data': {
+        #             'nodes': nodes,
+        #             'links': links
+        #         }
+        #     }
+        # )
+        graph_state = await self.get_graph_state()
+        
+        await self.send(text_data=json.dumps(graph_state, cls=DjangoJSONEncoder))
 
     async def graph_update(self, event):
         data = event['data']
         await self.send(text_data=json.dumps(data, cls=DjangoJSONEncoder))
 
     @database_sync_to_async
-    def save_graph_state(self, nodes, links):
-        Node.objects.all().delete()  # Clear existing nodes
-        Link.objects.all().delete()  # Clear existing links
-        # Save nodes
-        for node in nodes:
-            Node.objects.create(id=node['id'], x=node['x'], y=node['y'])
-        # Save links
-        for link in links:
-            source_node = Node.objects.get(id=link['source'])
-            target_node = Node.objects.get(id=link['target'])
+    def add_link(self, target):
+        # from ipdb import set_trace
+        # set_trace()
+        source_node, source_error = Node.objects.get_or_create(id=self.username, defaults={"x":randint(10, self.width), "y":randint(10, self.height)})
+        target_node, target_error = Node.objects.get_or_create(id=target, defaults={"x":randint(10, self.width), "y":randint(10, self.height)})
+        if not source_error and not target_error:
             Link.objects.create(source=source_node, target=target_node)
 
     @database_sync_to_async
     def get_graph_state(self):
-        nodes = list(Node.objects.values('id', 'x', 'y'))
-        links = list(Link.objects.values('source', 'target'))
         return {
-            'nodes': nodes,
-            'links': links
+            'nodes': list(Node.objects.values('id', 'x', 'y')),
+            'links': list(Link.objects.values('source', 'target'))
         }
 
     async def send_graph_state(self):
@@ -88,6 +87,5 @@ class GraphConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps(graph_state, cls=DjangoJSONEncoder))
     
     @database_sync_to_async
-    def add_node(self, username):
-        if not Node.objects.filter(id=username).exists():
-            Node.objects.create(id=username, x=randint(10, 1000), y=randint(10, 1000))
+    def add_node(self):
+        return Node.objects.get_or_create(id=self.username, defaults={"x":randint(10, self.width), "y":randint(10, self.height)})
